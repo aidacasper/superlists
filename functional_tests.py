67571c37c2ed3f1850a0a570feb72a7e35d0028e
Aidan Jasperson <aidan.jasperson@outlook.com>
2022-10-16 13:32:30 -0700

    Functional test working for only one question. About to refactor

--- functional_tests.py
+++ functional_tests.py
@@ -38,17 +38,27 @@ class NewVisitorTest(unittest.TestCase):
 
         table= self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertTrue(
-                any(row.text == '1: Buy peacock feathers' for row in rows),
-                "New to-do item did not appear in table"
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # There is still text inviting her to add another to-do item, she enters
         # 'Make lure with peacock feathers'
-        self.fail('Finish the test')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # The page updates again and both of her to-do items are listed
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
+        )
 
+        # One wonders whether the site will remember the list. 
+        self.fail('Finish the test!')
+        
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
